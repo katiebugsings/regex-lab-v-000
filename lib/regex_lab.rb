4fbc8639390e86bed57b4af646251dbcb50020e4
Katie Cassell <kcassell944@gmail.com>
2018-02-28 19:21:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*\W$/)
+  new_match = text.match(/^[A-Z].*\W$/)
+  if new_match
+    return true
+  else
+    return false
+  end
 
 end
 
